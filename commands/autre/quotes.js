b985fb11c880869ae4ffbca1e78d851064f99b61
Florian <f.schaessens@gmail.com>
2024-06-23 12:06:01 +0200

    add quote slash command

--- commands/autre/quotes.js
+++ commands/autre/quotes.js
@@ -1,24 +1,24 @@
 import { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } from 'discord.js';
-import funFactsAboutChickens from "../../datas/funFacts.js"
+import quotesData from "../../datas/quotesData.js"
 import path from 'path'
 
 
 export const data = new SlashCommandBuilder()
-.setName('chickenfact')
-.setDescription('Fournit un fait sur les poulets...')
+.setName('quotes')
+.setDescription("Des citations motivante pour te donner de l'inspiration")
 
 export const autre = true
 
 export const execute = async (interaction) => {
-    const randomFact = funFactsAboutChickens[Math.floor(Math.random() * funFactsAboutChickens.length)];
-    const imagePath = path.resolve('public', 'poulet.jpg');
+    const randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
+    const imagePath = path.resolve('public', 'remember.jpg');
     const attachment = new AttachmentBuilder(imagePath);
     
     const embed = new EmbedBuilder()
     .setColor(`#a306b6`)
-    .setTitle(`Chicken Facts   🐔`)
-    .setDescription(randomFact)
-    .setThumbnail('attachment://poulet.jpg')
+    .setTitle(`Chicken Quote   🐔`)
+    .setDescription(randomQuote)
+    .setThumbnail('attachment://remember.jpg')
 
 
     await interaction.reply({
